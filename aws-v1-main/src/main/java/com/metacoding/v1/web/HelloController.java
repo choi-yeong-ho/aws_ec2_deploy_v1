545c8c6bb2f03ab3c8c2917530f4d16ce99f6872
cyh <addcoss05@gmail.com>
2024-05-23 10:51:42 +0900

    2번째 수정

--- aws-v1-main/src/main/java/com/metacoding/v1/web/HelloController.java
+++ aws-v1-main/src/main/java/com/metacoding/v1/web/HelloController.java
@@ -13,14 +13,16 @@ public class HelloController {
 
     @GetMapping("/aws/v1")
     public String hello(@RequestParam(defaultValue = "1") Integer number){
+		String str = "<h1>number 값이 "+number+"로 요청했습니다.</h1>";
         if(number == 1){ // info 로그
             log.info("/aws/v1 이 호출되었어요. info 로그 #####################################");
         }else if(number == -1){ // error 로그
             log.error("/aws/v1 이 호출되었어요. error 로그 #####################################");
         }else if(number == 0){ // warn 로그
             log.warn("/aws/v1 이 호출되었어요. warn 로그 #####################################");
+			str = "<h2 style='color:blue'> ubuntu server 설치중... </h2>";
         }
-		String str = "<h1>number 값이 "+number+"로 요청했습니다.</h1>";
+		
         //return "<h2>aws v1</h2>";
 		return str;
     }
